3c9424bd216fa94450f7b439e9757030b3d3d8bf
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-10-16 09:20:34 +0900

    ss

--- server/internal/interfaces/handlers/task.go
+++ server/internal/interfaces/handlers/task.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/morninng/z-go-react-monorepo/internal/application"
+	"github.com/morninng/z-go-react-monorepo/internal/domain/entity"
 	"github.com/morninng/z-go-react-monorepo/internal/dto"
 )
 
@@ -21,6 +22,7 @@ func (h *TaskHandler) Routes(e *echo.Echo) {
 	fmt.Println("member route")
 
 	e.GET("/tasks", h.getTasks)
+	e.POST("/tasks", h.postTasks)
 
 }
 
@@ -31,6 +33,30 @@ func (h *TaskHandler) getTasks(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
+	response := make(dto.Tasks, len(*tasks))
+	for i, task := range *tasks {
+		response[i] = dto.Task{
+			Id:       task.ID,
+			Name:     task.Name,
+			Constent: task.Content,
+		}
+	}
+
+	return c.JSON(http.StatusOK, &response)
+}
+
+func (h *TaskHandler) postTasks(c echo.Context) error {
+	// ctx := c.Request().Context()
+
+	tasks := &entity.Tasks{}
+	if err := c.Bind(tasks); err != nil {
+		fmt.Println("ssss", err)
+		return c.JSON(http.StatusBadRequest, dto.BadRequestError{
+			Code:    dto.BadRequestErrorCodeEnumInvalidParameter,
+			Message: "sss",
+		})
+	}
+
 	response := make(dto.Tasks, len(*tasks))
 	for i, task := range *tasks {
 		response[i] = dto.Task{
